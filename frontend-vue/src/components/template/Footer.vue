12fa03bcbc07f8ea5cdead5e266fa66667d4c132
Breno Batista <breno_1993@outlook.com>
2023-08-16 03:23:22 -0300

    feat: nav e menu toggle

---
+++ frontend-vue/src/components/template/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+        <footer class="footer">
+            <span>&copy; 2023 <strong>Brenekat</strong></span>
+        </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+    
+</script>
+
+<style lang="css" scoped>
+    .footer{
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
---
+++ frontend-vue/src/components/template/Header.vue
@@ -0,0 +1,80 @@
+<template>
+    <header class="header">
+        <a class="toggle" @click="toggleMenu" >
+            <i class="toggle-menu-icon">
+                <font-awesome-icon :icon="open" />
+            </i>
+        </a>
+        <h1 class="title">
+            <a href="">{{ title }}</a>
+        </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: {
+        title: String,
+        hideToggle: Boolean
+    },
+    // data() {
+    //     return {
+    //         isActive: false,
+    //     };
+    // },
+    computed: {
+        open() {
+            return this.$store.state.isMenuVisible ? "fa-solid fa-x" : "fa-solid fa-bars"
+            // return this.isActive ? "fa-solid fa-bars" : "fa-solid fa-x"
+        }
+    },
+    methods: {
+        toggleMenu() {
+            return (
+                // this.isActive = this.isActive ? false : true,
+                this.$store.commit("toggleMenu")
+            )
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.header {
+    grid-area: header;
+    background: linear-gradient(to right, #0f0c29, #302b63);
+    /* color: #fff; */
+    /* text-align: center; */
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.title {
+        font-size: 1.2rem;
+        color: #fff;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+    }
+header.header > a.toggle{
+    cursor: pointer;
+    width: 60px;
+        height: 100%;
+        color: #fff;
+        text-decoration: none;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+}
+.title a {
+    cursor: pointer;
+    color: #fff;
+    text-decoration: none;
+}
+header.header > a.toggle:hover {
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+</style>
