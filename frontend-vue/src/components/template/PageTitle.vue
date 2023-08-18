98e65d9fc4d6cd0f59386eccc4c67edbe07d6685
Breno Batista <breno_1993@outlook.com>
2023-08-18 14:46:11 -0300

    feat: criando component pagetitle
    component para atualizar titulo e subtítulo do component content conforme o conteúdo que se navega

---
+++ frontend-vue/src/components/template/PageTitle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="page-title">
+        <h1>
+            <i v-if="open" :class="open" ></i>
+            {{ main }}
+        </h1>
+        <h2>{{ sub }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PageTitle",
+        props: ['main', 'sub', 'open']
+    }
+</script>
+
+<style>
+    page-title h1{
+        margin: 0px;
+    }
+
+    page-title h2{
+        color: #777;
+        margin-top: 5px;
+        font-size: 1.3rem;
+    }
+</style>
